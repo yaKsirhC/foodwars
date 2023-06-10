123998a06ccb43f6f9ef43a486f8f67641fbcc76
vihdutta <vihdutta@gmail.com>
2023-06-10 16:31:08 -0400

    pixi.js and node.js basic setup

--- public/index.js
+++
@@ -1,5 +0,0 @@
-const socket = io("ws://localhost:8000");
-
-socket.on("connect", () => { 
-    console.log("socket", socket.id, "connected");
-});
--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const { createServer } = require('http');
 const { Server } = require('socket.io');
 
@@ -7,10 +6,16 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
+console.log("server started");
 io.on('connection', (socket) => { 
     console.log("socket", socket.id, "connected");
 });
 
-const port = 8000;
+const test = require("./test.js");
+
+const port = 6969;
 app.use(express.static('public'));
 httpServer.listen(port);
+
+console.log(__dirname)
+console.log(__filename)
