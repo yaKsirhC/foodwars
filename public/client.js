4f389329cf88fbd71677cd92fb7173c72d0bf908
vihdutta <vihdutta@gmail.com>
2023-06-11 01:40:28 -0400

    added basic movement

--- public/client.js
+++ public/client.js
@@ -1,23 +1,75 @@
+const Application = PIXI.Application;
+
 const socket = io("ws://localhost:6969");
 socket.on("connect", () => { 
     console.log("socket", socket.id, "connected");
 });
 
-// Create a PixiJS application
-var app = new PIXI.Application({
-    width: 800,
-    height: 600,
-    backgroundColor: 0xAAAAAA
-});
+// PIXI
+const app = new Application({
+    width: 500,
+    height: 500,
+    transparent: false,
+    antialias: true,
+})
 
-// Add the PixiJS canvas to the HTML document
+app.renderer.backgroundColor = 0x23395D;
+app.renderer.resize(window.innerWidth, window.innerHeight);
+app.renderer.view.style.position = "absolute";
+
+const Graphics = PIXI.Graphics;
+const rectangle = new Graphics();
+rectangle.beginFill(0xAA33BB)
+.drawRect(200, 200, 100, 120)
+.endFill();
+
+// Create a keyboard state object to keep track of the key states
+const keyboard = {
+    w: false,
+    a: false,
+    s: false,
+    d: false,
+  };
+  
+  window.addEventListener("keydown", handleKeyDown);
+  window.addEventListener("keyup", handleKeyUp);
+  
+  // Key event handler
+  function handleKeyDown(event) {
+    const key = event.key.toLowerCase();
+    if (keyboard.hasOwnProperty(key)) {
+      keyboard[key] = true;
+    }
+  }
+  
+  function handleKeyUp(event) {
+    const key = event.key.toLowerCase();
+    if (keyboard.hasOwnProperty(key)) {
+      keyboard[key] = false;
+    }
+  }
+  
+// While game is running
+app.ticker.add(() => {
+    if (keyboard.w) {
+      rectangle.y -= 5;
+    }
+  
+    if (keyboard.a) {
+      rectangle.x -= 5;
+    }
+  
+    if (keyboard.s) {
+      rectangle.y += 5;
+    }
+  
+    if (keyboard.d) {
+      rectangle.x += 5;
+    }
+
+  });
+  
+// put on the canvas
 document.body.appendChild(app.view);
+app.stage.addChild(rectangle);
 
-// Create a background rectangle
-var background = new PIXI.Graphics();
-background.beginFill(0xFF0000); // Set the background color (replace 0xFF0000 with your desired color)
-background.drawRect(0, 0, app.screen.width, app.screen.height);
-background.endFill();
-
-// Add the background to the stage
-app.stage.addChild(background);
